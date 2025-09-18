00bf09624e3cb0d72c0ebae4a3def738851ece7b
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-09-18 20:13:49 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.secret_key = 'troque-por-uma-chave-secreta-aqui'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    error = None
     name = None
+    institution = None
+    discipline = None
 
     if request.method == 'POST':
-        name = request.form.get('name', '').strip()
-        if not name:
-            error = '☛ Preencha este campo'
-        else:
-            return render_template('index.html', name=name)
+        if request.form.get('name'):
+            name = request.form.get('name').strip()
+        if request.form.get('institution'):
+            institution = request.form.get('institution').strip()
+        if request.form.get('discipline'):
+            discipline = request.form.get('discipline').strip()
 
-    return render_template('index.html', name=None, error=error)
+    return render_template(
+        'index.html',
+        name=name,
+        institution=institution,
+        discipline=discipline
+    )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
